5d8d2cfe15aea4316d5b53b95af25ec6e6c6b6f5
Angel Arturo <angelfaridarturo@gmail.com>
2022-02-28 12:04:01 -0500

    about me left panel

---
+++ AboutMe.php
@@ -0,0 +1,75 @@
+<?php include("includes\header.php") ?>
+<div class="container p-4">
+    <!-- BUTTONS CARD -->
+    <div class="card card-body">
+        <div class="btn-group">
+            <a href="curriculum.php" class="btn btn-outline-light"style="background-color: #12657E;"> Projects</a>
+            <a href="#" class="btn btn-outline-light" style="background-color: #12657E;">Galery</a>
+            <a href="#" class="btn btn-outline-light" style="background-color: #12657E;">Social Media</a>
+            <a href="aboutme.php" class="btn btn-outline-light" style="background-color: #065785;"
+            aria-current="page">About me</a>
+        </div>
+    </div>
+    <!--Personal Info Card-->
+    <div class="row">
+        <div class="col-md-3">
+            <div class="card card-body" style="background-color: #065785;">
+            <!--head section-->
+                <img src="includes\profilephoto.png" class="rounded">
+                <p class="text-white text-decoration-underline fw-bold fs-4">About me</p>
+                <p class="text-white fs-6">I am software and automatization engeniering student, 
+                    tecnology lover and a person who is in a constant desire to learn 
+                    and searching ideas which can evolve to busines oportunities. 
+                    I am currently looking for job opportunities</p>
+                <!--Languages section-->
+                <p class="text-white text-decoration-underline fw-bold fs-4">Languages</p>
+                <p class="text-white fs-6">Spanish</p>
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar"
+                    style="width: 100%; background-color:#004384;"
+                    aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <p class ="text-white fs-6">English</p>
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar"
+                    style="width: 60%; background-color:#004384;"
+                    aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <!--Contact section-->
+                <p class="text-white text-decoration-underline fw-bold fs-4">Contact</p>
+                <div class="text text-white fw-6">
+                    <a href="mailto:angelfaridarturo@gmail.com" style="color:white">
+                        <p><i class="fa-solid fa-envelope"></i> e-Mail:
+                        angelfaridarturo@gmail.com</p>
+                    </a>
+                    <p><i class="fa-solid fa-location-dot"></i> Ubication:
+                    Popayán-Cauca-Colombia</p>
+                    <p><i class="fa-solid fa-phone"></i> Phone: 
+                    +57 3128014933</p>
+                </div>
+                <!--hobbies section-->
+                <p class="text-white text-decoration-underline fw-bold fs-4">Interest and hobbies</p>
+                <div class="text text-white fw-6">
+                Keeping learning about development. I'm a sport, movie and videogame lover
+                </div>
+                <!--Social media section-->
+                <p class="text-white text-decoration-underline fw-bold fs-4">Social Media</p>
+                <div class="text text-white fw-6">
+                    <a href="https://www.linkedin.com/in/angel-arturo-b6761b1b2/" style="color: white" target="_blank">
+                        <p><i class="fa-brands fa-linkedin"></i> : Angel Arturo</p>
+                    </a>
+                    <a href="https://github.com/Murdock9607" style="color: white" target="_blank">
+                        <p><i class="fa-brands fa-github"></i> : Murdock9607</p>
+                    </a>
+                    <a href="https://www.instagram.com/farid_arturo/"style="color: white" target="_blank">
+                        <p><i class="fa-brands fa-instagram"></i> : farid_arturo</p>
+                    </a>
+                    <a href="https://twitter.com/farid_arturo"style="color: white" target="_blank">
+                        <p><i class="fa-brands fa-twitter"></i> : farid_arturo</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include("includes\hooter.php") ?>
---
+++ includes/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ANGEL ARTURO CURRICULUM</title>
+    <!--bootstrap 4-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" 
+    crossorigin="anonymous">
+    <!--font awesome -->
+    <script src="https://kit.fontawesome.com/97f6b1d0cf.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<nav class="navbar navbar-dark" style="background-color: #12657e;">
+    <div class="container">
+         <a href="curriculum.php" class="navbar-brand">
+         <i class="fa-solid fa-laptop-code"></i>
+         ANGEL ARTURO - CURRICULUM
+         </a>
+         <div class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-success" style="background-color: #0C8A62;"
+            type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+         </div>
+         
+    </div>
+</nav>
